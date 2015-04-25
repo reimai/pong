fe0c86854a21f8c9cbefe68157c820dd89112cc4
rei <naugadina@gmail.com>
2015-04-26 02:24:37 +0300

    it works

--- src/Main.hs
+++ src/Main.hs
@@ -2,92 +2,159 @@ module Main where
 
 import System.Timeout
 import System.Console.ANSI
-import System.Console.Terminal.Size
+import qualified System.Console.Terminal.Size as T
+import System.Console.Terminal.Size (Window)
 import Data.Maybe
+import qualified Data.Text.IO as TextIO
+import qualified Data.Text as Text
 import Control.Monad
+import Control.Concurrent
 import System.IO
 import PData as P
 import Utils
+import Control.Concurrent
+import System.CPUTime
+import Control.Exception
 
-main = do 
-	hSetBuffering stdin NoBuffering --get input immedietly 
-	hSetEcho stdin False		--don't show the typed character
-	jwindow <- size
+main = do
+	hSetBuffering stdin NoBuffering --get input immedietly
+	hSetBuffering stdout NoBuffering 
+	hSetEcho stdin False            --don't show the typed character
+	jwindow <- T.size
 	let window = fromJust jwindow
-	let pd = initPaddle 1 ((width window - paddleWidth) `div` 2) (height window - 1)
-	let comp = initPaddle 2 ((width window - paddleWidth) `div` 2) (0)
-	let ball = Ball $ Position (Coords 0 0) None 
-	gameLoop stdin window $ World pd comp ball 
+	let center = ((T.width window - pdWidth) `div` 2)
+	let pd = initPaddle 1 center (T.height window - 2) --the lowest line of screen is taken by input
+	let comp = initPaddle 2 center (0)
+	let ball = Ball $ Position (Coords center 5) $ Direction HLeft VDown 
+	gameLoop stdin window $ World pd comp ball
 
 gameLoop :: Handle -> Window Int -> World -> IO()
-gameLoop h wnd world = do 
-			renderWorld h wnd world 
-			jch <- timeout (floor(1/fps * 10^6)) (hGetChar h)
-			let ch = fromMaybe ' ' jch
-			when (ch /= 'q') $ gameLoop h wnd $ moveWorld ch wnd world 
-				where fps = 60
+gameLoop h wnd world = do
+			renderWorld wnd world
+			e <- threadDelay (floor(1/fps * 10^6))  
+			ch <- readAll h ' '
+			when (ch /= 'q') $ gameLoop h wnd $ moveWorld ch wnd world
+				where fps = 20
 
-moveWorld :: Char -> Window Int -> World -> World		        	
-moveWorld ch wnd (World pd comp ball) = World (newPd ch) (newPd $ compTurn ch) ball
-				where newPd ch = movePd wnd pd $ parseDir ch
+--reads all input from Handle, returns only the last char
+readAll :: Handle -> Char -> IO(Char)
+readAll h ch = hReady h >>= \gotIt -> if' gotIt (hGetChar h >>= readAll h) $ return ch
 
-renderWorld :: Handle -> Window Int -> World -> IO ()
-renderWorld h wnd (World pd comp ball) = do
+moveWorld :: Char -> Window Int -> World -> World
+moveWorld ch wnd (World pd comp ball) = World newHuman newComp newBall 
+				where 	newHuman = movePd wnd pd $ parseDir ch 
+					newComp = movePd wnd comp $ compTurn wnd comp ball
+				  	newBall = moveBall pd comp wnd ball
+
+renderWorld :: Window Int -> World -> IO ()
+renderWorld wnd (World pd comp ball) = do
 			clearScreen
-			putStrLn $ render wnd comp 
-			--putStrLn $ replicate (y (bpos ball)) '\n'
-			--putStrLn $ render wnd ball
-			putStrLn $ replicate (height wnd - 5) '\n' --wtf is 5?
-			putStrLn $ render wnd pd
+			putStr $ render wnd comp-- ++ " " ++ show (pos comp)
+			putStr $ replicate (getY ball) '\n'
+			putStr $ render wnd ball-- ++ " " ++ show (pos ball)
+			putStr $ replicate (getY pd - getY ball) '\n'
+			putStrLn $ render wnd pd-- ++ " " ++ show (pos pd)
 
-compTurn ch = ch
+compTurn :: Window Int -> Paddle -> Ball -> Direction 
+compTurn wnd comp ball 	| caught comp futureBall = dNone 
+		   	| otherwise = if' (getX futureBall < getX comp) (Direction HLeft VNone) (Direction HRight VNone)  
+				where futureBall = Ball $ Position (move (back $ getDir futureBall0) 1 (coord.pos $ futureBall0)) $ getDir futureBall0 				 
+					where futureBall0 = predictBall wnd ball				 
+
+--ball position on the upper (0) row if human player catch it
+predictBall :: Window Int -> Ball -> Ball
+predictBall wnd ball =  getFin.turnUp.getFin $ ball
+			where 	turnUp ball = newBall (getX ball) (getY ball) $ collide (getDir ball) (Direction HNone VDown)
+				getFin ball = flip $ move $ correct ball
+					where 	correct ball 	| isUp $ getDir ball 	= newBall (getX ball) ((getY ball) - (highGlass wnd)) $ getDir ball
+				 		 		| isDown $ getDir ball	= newBall (getX ball) ((getY ball) + (highGlass wnd)) $ getDir ball
+								| otherwise = ball
+
+						move ball 	| isLeft $ getDir ball 	= newBall ((getX ball) - (distY ball)) (moveY ball) $ getDir ball
+							 	| isRight $ getDir ball	= newBall ((getX ball) + (distY ball)) (moveY ball) $ getDir ball
+								| otherwise = ball
+							where 	distY ball	| isUp $ getDir ball 	= getY ball
+										| otherwise 		= T.height wnd - 1 - (getY ball)								
+								moveY ball	| isUp $ getDir ball 	= 0
+										| otherwise 		= T.height wnd - 1 
+					
+						flip ball	| getX ball <= 0 	   	= newBall (-(getX ball)) (getY ball) $ collide (getDir ball) (Direction HLeft VNone)
+								| getX ball >= T.width wnd - 1 	= newBall (2 * (T.width wnd) - (getX ball)) (getY ball) $ collide (getDir ball) (Direction HRight VNone) 
+								| otherwise = ball
+								
+
+newBall :: Int -> Int -> Direction -> Ball
+newBall x y dir = Ball $ Position (Coords x y) dir 
+
+--skip a long way through high glass - correct y coord by N window widths if it is < height
+highGlass :: Window Int -> Int
+highGlass wnd = T.height wnd `div` (T.width wnd) * (T.width wnd)   	
 
 movePd :: Window Int -> Paddle -> Direction -> Paddle
-movePd _ pd None = pd
-movePd wnd pd dir = fitWindow wnd $ Paddle (P.id pd) $ movePos (pos pd) dir $ getGap pd
+movePd _ pd (Direction HNone VNone) = pd
+movePd wnd pd dir = fitWindow wnd $ Paddle (P.id pd) $ movePos dir (getGap pd) (pos pd)
 
+moveBall :: Paddle -> Paddle -> Window Int -> Ball -> Ball
+moveBall pd comp wnd ball	| getY newBall == getY comp + 1 && not (caught comp newBall) = stop newBall
+			        | getY newBall == getY pd - 1 && not (caught pd newBall) = stop newBall
+			        | otherwise = newBall
+			        	where newBall = Ball $ (movePos ((collision ball $ findCollision pd comp wnd ball)) ballSpeed (pos ball))
+
+collision :: Ball -> Direction -> Direction
+collision ball (Direction HNone VNone) = dir.pos $ ball
+collision ball obst = collide (dir.pos $ ball) obst
+
+findCollision :: Paddle -> Paddle -> Window Int -> Ball -> Direction
+findCollision pd comp wnd ball 	| getX ball == 0 		= Direction HLeft VNone
+				| getX ball == T.width wnd - 1 	= Direction HRight VNone
+				| getY ball == getY comp + 1 	= Direction HNone VUp
+				| getY ball == getY pd - 1	= Direction HNone VDown
+				| otherwise 			= dNone
+ballSpeed :: Int
+ballSpeed = 1
+
+caught :: Paddle -> Ball -> Bool
+caught pd ball = x >= pdX - 1 && x <= pdX + pdWidth + 1
+	where 	x = getX ball
+		pdX = getX pd
+
+stop :: Ball -> Ball
+stop ball = Ball $ Position (coord.pos $ ball) dNone
+
+--some fake inertia
 getGap :: Paddle -> Int
-getGap pd 	| (dir $ pos pd) == None = 2
+getGap pd 	| (dir $ pos pd) == dNone = 2
 		| otherwise = 4
 
 fitWindow :: Window Int -> Paddle -> Paddle
 fitWindow wnd pd = fitY wnd $ fitX wnd pd
 	where
-		fitX wnd pd 	| pdx pd < 0 = Paddle (P.id pd) $ Position (Coords 0 $ pdy pd) None
-				| pdx pd + paddleWidth > width wnd = Paddle (P.id pd) $ Position (Coords (width wnd - paddleWidth) $ pdy pd) None
+		-- 1 is not a bug - it's a feature - or the game could last forever
+		fitX wnd pd 	| getX pd < 1 = Paddle (P.id pd) $ Position (Coords 1 $ getY pd) dNone
+				| getX pd + width pd > T.width wnd = Paddle (P.id pd) $ Position (Coords (T.width wnd - width pd) $ getY pd) dNone
 	   			| otherwise = pd
- 
-		fitY wnd pd 	| pdy pd < 0 = Paddle (P.id pd) $ Position (Coords (pdx pd) 0) None
-				| pdy pd  + 1 > height wnd = Paddle (P.id pd) $ Position (Coords (pdx pd) (height wnd - 1)) None
+
+		fitY wnd pd 	| getY pd < 0 = Paddle (P.id pd) $ Position (Coords (getX pd) 0) dNone
+				| getY pd  + 2 > T.height wnd = Paddle (P.id pd) $ Position (Coords (getX pd) (T.height wnd - 2)) dNone
 				| otherwise = pd
+
 --a quick way to get coords
-pdy :: Paddle -> Int
-pdy pd = y $ coord $ pos pd 
-pdx :: Paddle -> Int
-pdx pd = x $ coord $ pos pd 
+getY :: Renderable a => a -> Int
+getY r = y.coord.pos $ r
+getX :: Renderable a => a -> Int
+getX r = x.coord.pos $ r
+getDir :: Renderable a => a -> Direction
+getDir r = dir.pos $ r
 
-movePos :: Position -> Direction -> Int -> Position 
-movePos pos dir gap = Position (move (coord pos) dir gap) dir
-
-move :: Coords -> Direction -> Int -> Coords
-move crd P.Up gap = Coords (x crd) (y crd - gap)
-move crd P.Down gap = Coords (x crd) (y crd + gap)
-move crd P.Left gap = Coords (x crd - gap) (y crd)
-move crd P.Right gap = Coords (x crd + gap) (y crd)
-move crd P.None _ = crd
 
 parseDir :: Char -> Direction
-parseDir 'D' = P.Left 				
-parseDir 'A' = P.Up 				
-parseDir 'C' = P.Right 				
-parseDir 'B' = P.Down
-parseDir _ = P.None
+parseDir 'D' = Direction HLeft VNone
+--parseDir 'A' = up
+parseDir 'C' = Direction HRight VNone
+--parseDir 'B' = down
+parseDir _ = dNone
 
 initPaddle :: Int -> Int -> Int -> Paddle
-initPaddle id x y = Paddle id $ Position (Coords x y) $ None 
-
-render :: Window Int -> Paddle -> String
-render wnd pd	| (width wnd) < paddleWidth = error "that's a wee window"
-	  	| otherwise = replicate (pdx pd) ' ' ++ show pd
+initPaddle id x y = Paddle id $ Position (Coords x y) $ dNone
 
 
--- src/PData.hs
+++ src/PData.hs
@@ -1,20 +1,104 @@
 module PData where
 
+import System.Console.ANSI
+import qualified System.Console.Terminal.Size as T
+import System.Console.Terminal.Size (Window)
+import Utils
+
+class (Show a) => Renderable a where
+	width :: a -> Int
+	pos :: a -> Position
+	render :: Window Int -> a -> String
+	render wnd rndbl   	| (T.width wnd) < width rndbl = error "that's a wee window"
+                 		| otherwise = replicate (x.coord.pos $ rndbl) ' ' ++ show rndbl
+
 data World = World { human :: Paddle, comp :: Paddle, ball :: Ball} 
 	
 data Paddle = Paddle { 	id :: Int,
-			pos :: Position} 
-paddleWidth = 7
+			ppos :: Position} 
+
+pdWidth :: Int
+pdWidth = 9
 
 instance Show Paddle where
-	show _ = replicate paddleWidth '█'
+	show p = replicate (width p) '█'
 
-data Position = Position {coord :: Coords, dir :: Direction } deriving Show
+instance Renderable Paddle where
+	width _ = pdWidth 
+	pos = ppos
 
-data Coords = Coords { x :: Int, y :: Int } deriving Show
 
-data Direction = Up | Down | Left | Right | None deriving (Show, Eq)
+data Position = Position {coord :: Coords, dir :: Direction } 
+instance Show Position where 
+	show p = show (coord p) ++ " " ++ show (dir p)
+
+movePos :: Direction -> Int -> Position -> Position
+movePos dir gap pos = Position (move dir gap (coord pos)) dir 	
+
+
+data Coords = Coords { x :: Int, y :: Int } 
+instance Show Coords where
+	show (Coords x y) = show x ++ ":" ++ show y
+
+
+data VDirection = VUp | VDown | VNone deriving (Show, Eq)
+data HDirection = HRight | HLeft | HNone deriving (Show, Eq)
+
+data Direction = Direction {h :: HDirection, v :: VDirection} deriving (Show, Eq)
+dNone :: Direction
+dNone = Direction HNone VNone
+
+move :: Direction -> Int -> Coords -> Coords
+move (Direction h v) gap crd = Coords (moveH h gap crd) (moveV v gap crd)
+
+moveV :: VDirection -> Int -> Coords -> Int
+moveV VUp gap crd = y crd - gap
+moveV VDown gap crd = y crd + gap
+moveV VNone _ crd = y crd
+
+moveH :: HDirection -> Int -> Coords -> Int
+moveH HRight gap crd = x crd + gap 
+moveH HLeft gap crd = x crd - gap
+moveH HNone _ crd = x crd 
+
+isUp :: Direction -> Bool
+isUp (Direction _ VUp) = True
+isUp _ = False
+
+isDown :: Direction -> Bool
+isDown (Direction _ VDown) = True
+isDown _ = False
+
+isRight :: Direction -> Bool
+isRight (Direction HRight _) = True
+isRight _ = False
+
+isLeft :: Direction -> Bool
+isLeft (Direction HLeft _) = True
+isLeft _ = False
+
+collide :: Direction -> Direction -> Direction
+collide (Direction h v) (Direction obstH obstV) = Direction newH newV
+		where	newV = if' (v == obstV) (backV v) v
+			newH = if' (h == obstH) (backH h) h
+
+back :: Direction -> Direction
+back (Direction h v) = Direction (backH h) (backV v)
+
+backV :: VDirection -> VDirection
+backV VUp = VDown
+backV VDown = VUp
+backV VNone = VNone
+
+backH :: HDirection -> HDirection
+backH HRight = HLeft
+backH HLeft = HRight
+backH HNone = HNone
 
 data Ball = Ball { bpos :: Position } 
 instance Show Ball where 
 	show _ = "●"
+
+instance Renderable Ball where
+	width _ = 1
+	pos = bpos
